36653b7769deb6ac12ce422d2ced788786759496
Дмитро Примаченко <dmitroprimacenko@MacBook-Air-Dmitro-2.local>
2025-02-27 16:50:57 +0200

    fix scroll blog

--- src/pages/home/components/sliderReviews/SliderReviews.jsx
+++ src/pages/home/components/sliderReviews/SliderReviews.jsx
@@ -22,6 +22,7 @@ const ReviewCardWrapper = styled.div`
 
 const ReviewCard = styled.div`
   width: 280px !important;
+  min-width: 0;
   height: 410px;
   background: #111;
   padding: 20px;
@@ -192,7 +193,7 @@ const SliderReviews = () => {
     beforeChange: (oldIndex, newIndex) => setCurrentSlide(newIndex),
     responsive: [
       {
-        breakpoint: 1600, // Для великих екранів (1600px+)
+        breakpoint: 2600,
         settings: {
           slidesToShow: 4,
           slidesToScroll: 1,
@@ -201,7 +202,16 @@ const SliderReviews = () => {
         },
       },
       {
-        breakpoint: 1280, // Десктоп (1280px+)
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          centerMode: false,
+          arrows: true,
+        },
+      },
+      {
+        breakpoint: 1280,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
@@ -210,7 +220,7 @@ const SliderReviews = () => {
         },
       },
       {
-        breakpoint: 1024, // Планшети (1024px+)
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2.5,
           slidesToScroll: 1,
@@ -220,7 +230,7 @@ const SliderReviews = () => {
         },
       },
       {
-        breakpoint: 768, // Маленькі планшети
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
@@ -230,7 +240,7 @@ const SliderReviews = () => {
         },
       },
       {
-        breakpoint: 576, // Великі мобільні
+        breakpoint: 576,
         settings: {
           slidesToShow: 1.5,
           slidesToScroll: 1,
@@ -240,7 +250,7 @@ const SliderReviews = () => {
         },
       },
       {
-        breakpoint: 450, // Середні мобільні
+        breakpoint: 450,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -250,7 +260,7 @@ const SliderReviews = () => {
         },
       },
       {
-        breakpoint: 375, // Маленькі мобільні
+        breakpoint: 375,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
