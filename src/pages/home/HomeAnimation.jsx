e86a29610d528fbe38a0e9f190ea758bd997c061
Дмитро Примаченко <dmitroprimacenko@MacBook-Air-Dmitro-2.local>
2025-02-27 16:25:51 +0200

    fix scroll

--- src/pages/home/HomeAnimation.jsx
+++ src/pages/home/HomeAnimation.jsx
@@ -5,15 +5,18 @@ import styled from 'styled-components'
 
 const SectionWrapper = styled.div`
   width: 100%;
+  min-height: 1px;
+  overflow: hidden;
   opacity: 0;
-  transform: translateY(50px);
+  transform: translate3d(0, 50px, 0);
+  will-change: opacity, transform;
   transition:
     opacity 0.6s ease-out,
     transform 0.6s ease-out;
 
   &.visible {
     opacity: 1;
-    transform: translateY(0);
+    transform: translate3d(0, 0, 0);
   }
 `
 
@@ -26,8 +29,10 @@ const FadeInSection = ({ children }) => {
   return (
     <SectionWrapper ref={ref} className={inView ? 'visible' : ''}>
       <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={inView ? { opacity: 1, y: 0 } : {}}
+        initial={{ opacity: 0, transform: 'translate3d(0, 50px, 0)' }}
+        animate={
+          inView ? { opacity: 1, transform: 'translate3d(0, 0, 0)' } : {}
+        }
         transition={{ duration: 0.6, ease: 'easeOut' }}
       >
         {children}
